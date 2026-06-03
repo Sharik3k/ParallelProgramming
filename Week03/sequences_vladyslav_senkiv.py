1ef3815ae6b7e246dbcb63853403219b7e7b76a9
Senkiv Vladyslav <vladyslav.senkiv@gmail.com>
2026-06-03 14:55:21 +0300

    Update sequences_vladyslav_senkiv.py

--- Week03/sequences_vladyslav_senkiv.py
+++ Week03/sequences_vladyslav_senkiv.py
@@ -1,17 +1,29 @@
 def remove_duplicates(seq: list) -> list:
     result = []
+
     for item in seq:
         if item not in result:
             result.append(item)
+
     return result
 
 
 def list_counts(seq: list) -> dict:
-    counts = {}
+    result = {}
+
     for item in seq:
-        counts[item] = counts.get(item, 0) + 1
-    return counts
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+
+    return result
 
 
 def reverse_dict(d: dict) -> dict:
-    return {value: key for key, value in d.items()}
+    result = {}
+
+    for key, value in d.items():
+        result[value] = key
+
+    return result
