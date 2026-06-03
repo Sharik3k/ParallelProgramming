af8267ab0a36d5b3ea792900b276b223e9f50a48
Senkiv Vladyslav <vladyslav.senkiv@gmail.com>
2026-06-03 14:54:17 +0300

    Update pyramid_vladyslav_senkiv.py

--- Week03/pyramid_vladyslav_senkiv.py
+++ Week03/pyramid_vladyslav_senkiv.py
@@ -1,10 +1,10 @@
 def calculate_pyramid_height(number_of_blocks):
     height = 0
-    i = 1
+    needed_blocks = 1
 
-    while number_of_blocks >= i:
-        number_of_blocks = number_of_blocks - i
+    while number_of_blocks >= needed_blocks:
+        number_of_blocks -= needed_blocks
         height += 1
-        i += 1
+        needed_blocks += 1
 
     return height
